a5ce173152447a3e29cd1a6e64681544b229fab1
Arihant416 <arihantjain416@gmail.com>
2020-06-01 15:16:07 +0530

    Passport

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -1,21 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var passportLocalMongoosse = require('passport-local-mongoose');
 
 var User = new Schema({
-   username: {
-      type: String,
-      unique: true,
-      required: true,
-   },
-   password: {
-      type: String,
-      required: true
-   },
+
    admin: {
       type: Boolean,
       default: false
    }
 })
+User.plugin(passportLocalMongoosse);
 
 module.exports = mongoose.model('User', User);
